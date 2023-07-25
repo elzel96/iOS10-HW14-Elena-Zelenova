3d22de3aac71f8956b7b1cf1f9f6cbe857810f41
elzel <elzel666@gmail.com>
2023-07-25 19:41:47 +0300

    Fixed Model

--- iOS10-HW14-Elena Zelenova/Sources/Model/Model.swift
+++ iOS10-HW14-Elena Zelenova/Sources/Model/Model.swift
@@ -13,40 +13,55 @@ struct Model {
     let number: String
 }
 
-extension Model {
+struct SectionModel {
+     var title: String
+     var buttonIsHiden: Bool = true
+     var item: [Model]
+ }
+
+class DataManager {
     
-    func createModels() -> [[Model]] {
+    private init() {}
+    
+    static let shared = DataManager()
+    
+    func createSectionModels() -> [SectionModel] {
         return [
-            [
-                Model(image: "recents", title: "Недавние", number: "483"),
-                Model(image: "favourites", title: "Избранное", number: "56"),
-                Model(image: "friends", title: "Друзья", number: "78"),
-                Model(image: "trips", title: "Поездки", number: "100"),
-                Model(image: "vacation", title: "Отпуск", number: "123"),
-                Model(image: "work", title: "Работа", number: "9"),
-                Model(image: "trip", title: "Путешествие", number: "6"),
-                Model(image: "Bali", title: "Бали", number: "5"),
-            ],
-            [
-                Model(image: "cat", title: "Моя кошка", number: "55"),
-                Model(image: "dog", title: "Моя собака", number: "20"),
-                Model(image: "sport", title: "Спорт", number: "3"),
-                Model(image: "food", title: "Рецепты", number: "12")
-            ],
-            [
-                Model(image: "video", title: "Видео", number: "400"),
-                Model(image: "person.crop.square", title: "Селфи", number: "4"),
-                Model(image: "livephoto", title: "Фото Live Photos", number: "300"),
-                Model(image: "cube", title: "Портреты", number: "57"),
-                Model(image: "pano", title: "Панорамы", number: "5"),
-                Model(image: "square.stack.3d.down.right", title: "Серии", number: "4")
-            ],
-            [
-                Model(image: "square.stack.3d.forward.dottedline", title: "Анимированные", number: "44"),
-                Model(image: "square.and.arrow.down", title: "Импортированные", number: "2"),
-                Model(image: "eye.slash", title: "Скрытые", number: "3"),
-                Model(image: "trash", title: "Недавно удаленные", number: "1200")
-            ]
+            SectionModel(title: "Мои альбомы", buttonIsHiden: false, item:
+                            [
+                                Model(image: "recents", title: "Недавние", number: "483"),
+                                Model(image: "favourites", title: "Избранное", number: "56"),
+                                Model(image: "friends", title: "Друзья", number: "78"),
+                                Model(image: "trips", title: "Поездки", number: "100"),
+                                Model(image: "vacation", title: "Отпуск", number: "123"),
+                                Model(image: "work", title: "Работа", number: "9"),
+                                Model(image: "trip", title: "Путешествие", number: "6"),
+                                Model(image: "Bali", title: "Бали", number: "5"),
+                            ]),
+            SectionModel(title: "Общие альбомы", buttonIsHiden: false, item:
+                            [
+                                Model(image: "cat", title: "Моя кошка", number: "55"),
+                                Model(image: "dog", title: "Моя собака", number: "20"),
+                                Model(image: "sport", title: "Спорт", number: "3"),
+                                Model(image: "food", title: "Рецепты", number: "12")
+                            ]),
+            SectionModel(title: "Типы медиафайлов", buttonIsHiden: true, item:
+                            [
+                                Model(image: "video", title: "Видео", number: "400"),
+                                Model(image: "person.crop.square", title: "Селфи", number: "4"),
+                                Model(image: "livephoto", title: "Фото Live Photos", number: "300"),
+                                Model(image: "cube", title: "Портреты", number: "57"),
+                                Model(image: "pano", title: "Панорамы", number: "5"),
+                                Model(image: "square.stack.3d.down.right", title: "Серии", number: "4")
+                            ]),
+            SectionModel(title: "Другое", buttonIsHiden: true, item:
+                            [
+                                Model(image: "square.stack.3d.forward.dottedline", title: "Анимированные", number: "44"),
+                                Model(image: "square.and.arrow.down", title: "Импортированные", number: "2"),
+                                Model(image: "eye.slash", title: "Скрытые", number: "3"),
+                                Model(image: "trash", title: "Недавно удаленные", number: "1200")
+                            ])
         ]
     }
 }
+
