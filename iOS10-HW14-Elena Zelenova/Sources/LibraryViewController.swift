ac6489e0c035de242d0188e5204d83ca7989dc16
elzel <elzel666@gmail.com>
2023-07-25 23:41:05 +0300

    Created tab bar

---
+++ iOS10-HW14-Elena Zelenova/Sources/LibraryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryViewController.swift
+//  iOS10-HW14-Elena Zelenova
+//
+//  Created by Helena on 25.07.2023.
+//
+
+import UIKit
+
+ class LibraryViewController: UIViewController {
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         view.backgroundColor = .systemCyan
+     }
+ }
